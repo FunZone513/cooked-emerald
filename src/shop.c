aca7b02e2e19dd2ac561c068f564a1edac2b08c0
Cykada <f.zone513@gmail.com>
2026-07-06 11:39:37 +1000

    fixed compile errors

--- src/shop.c
+++ src/shop.c
@@ -893,7 +893,6 @@ static void BuyMenuDrawMapMetatile(s16 x, s16 y, const u16 *src, u8 metatileLaye
             BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[2], offset1, offset2, src);
             BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[3], offset1, offset2, src + 4);
         }
-     }
     }
 }
 
