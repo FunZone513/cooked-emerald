a319699b30458f46d367092e1d20b2bc8ff31b7a
Cykada <f.zone513@gmail.com>
2026-07-08 21:22:58 +1000

    changed strength

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1024,7 +1024,8 @@ static bool8 ShouldJumpLedge(s16 x, s16 y, enum Direction direction)
 
 static bool8 TryPushBoulder(s16 x, s16 y, enum Direction direction)
 {
-    if (FlagGet(FLAG_SYS_USE_STRENGTH))
+    //* Strength isn't a HM, you can just do that now
+    if (TRUE) // (FlagGet(FLAG_SYS_USE_STRENGTH))
     {
         u8 objectEventId = GetObjectEventIdByXY(x, y);
 
