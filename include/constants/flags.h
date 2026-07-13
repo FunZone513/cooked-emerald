472e587906226ec3fcd1ec75a48d4becc31966f3
Cykada <f.zone513@gmail.com>
2026-07-13 11:58:22 +1000

    horizon port updates
    tweak some gfx to now use frlg sprites and added more to the beach part of the script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1511,7 +1511,8 @@
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
-#define FLAG_UNUSED_0x8E5                           (SYSTEM_FLAGS + 0x85) // Unused Flag
+//MARK: Misc Flags
+#define FLAG_BEAT_HORIZON_PORT_KIDS                 (SYSTEM_FLAGS + 0x85)
 #define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
 #define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
